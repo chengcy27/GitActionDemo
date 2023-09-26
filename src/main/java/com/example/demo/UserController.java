40307ba39bc61c0dd5cf53ea77efb567882e4140
Cheng <chengcy@27@gmail.com>
2023-09-26 14:36:29 +0800

    Change demo() return to cannot

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "Hello Test!";
+        return "Hello? Cannot!";
     }
     
 }
